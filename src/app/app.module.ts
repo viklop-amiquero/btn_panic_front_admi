6929c4469fe0b7bc87b6d68e7aaf9f15983a717c
viklop <viklop7025@gmail.com>
2025-05-06 11:27:33 -0500

    feat: Configurar las variables de desarrollo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { provideHttpClient } from '@angular/common/http'
 
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule],
-    providers: [],
+    providers: [provideHttpClient()],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
