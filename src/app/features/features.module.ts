eac1a3cb80589920c99432b6fa2974258a505772
viklop <viklop7025@gmail.com>
2025-05-07 12:57:51 -0500

    feat: Importar shared module en , featuremodule.

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
-import { FeaturesRoutingModule } from './features-routing.module';
-import { DashboardPageComponent } from './dashboard/pages/dashboard-page/dashboard-page.component';
-import { IndexPageComponent } from './dashboard/pages/index-page/index-page.component';
+import { FeaturesRoutingModule } from './features-routing.module'
+import { DashboardPageComponent } from './dashboard/pages/dashboard-page/dashboard-page.component'
+import { IndexPageComponent } from './dashboard/pages/index-page/index-page.component'
 import { FeatureLayoutComponent } from './layout/feature-layout/feature-layout.component'
+import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
     declarations: [
-    DashboardPageComponent,
-    IndexPageComponent,
-    FeatureLayoutComponent
-  ],
-    imports: [CommonModule, FeaturesRoutingModule],
+        DashboardPageComponent,
+        IndexPageComponent,
+        FeatureLayoutComponent,
+    ],
+    imports: [CommonModule, FeaturesRoutingModule, SharedModule],
 })
 export class FeaturesModule {}
