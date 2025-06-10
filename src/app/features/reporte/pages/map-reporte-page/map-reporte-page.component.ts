efb2fa4036689bcda30652d1dd40c6eb847c7687
viklop <viklop7025@gmail.com>
2025-06-10 12:06:16 -0500

    feat: Diseñar reloj.

--- src/app/features/reporte/pages/map-reporte-page/map-reporte-page.component.ts
+++ src/app/features/reporte/pages/map-reporte-page/map-reporte-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-map-reporte-page',
-  standalone: false,
-  templateUrl: './map-reporte-page.component.html',
-  styleUrl: './map-reporte-page.component.css'
+    selector: 'app-map-reporte-page',
+    standalone: false,
+    templateUrl: './map-reporte-page.component.html',
+    styleUrl: './map-reporte-page.component.css',
 })
-export class MapReportePageComponent {
+export class MapReportePageComponent implements OnInit {
+    currentTime: Date = new Date()
+    private timer: any
+    alarmActive: boolean = true
 
+    ngOnInit(): void {
+        this.timer = setInterval(() => {
+            this.currentTime = new Date()
+        }, 1000)
+    }
+
+    ngOnDestroy(): void {
+        if (this.timer) {
+            clearInterval(this.timer)
+        }
+    }
 }
