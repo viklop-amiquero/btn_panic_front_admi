d1ab224e0dfbaa38f2f245811c04f7163deb07d6
viklop <viklop7025@gmail.com>
2025-06-19 12:56:43 -0500

    feat: crear dashboard.

--- src/app/auth/models/dtos/RoleMenuAuth.dto.ts
+++ src/app/auth/models/dtos/RoleMenuAuth.dto.ts
@@ -6,6 +6,8 @@ export interface RoleMenuAuth {
     id: number
     role_id: number
     menu_clave: string
+    ruta?: string
+    icono?: string
     menu_id: number
     permiso_id: number
 }
--- src/app/features/dashboard/pages/index-page/index-page.component.ts
+++ src/app/features/dashboard/pages/index-page/index-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { PermissionService } from '../../../services/permission/permission.service'
+import { RoleMenuAuth } from '../../../../auth/models/dtos/RoleMenuAuth.dto'
+import { RolService } from '../../../seguridad/rol/services/rol.service'
 
 @Component({
     selector: 'app-index-page',
@@ -6,9 +9,21 @@ import { Component } from '@angular/core'
     templateUrl: './index-page.component.html',
     styleUrl: './index-page.component.css',
 })
-export class IndexPageComponent {
-    public breadcrumbs = 'usuarios'
-    public title = 'usuarios'
-    showButton = true
-    buttonLabel = 'Nuevo'
+export class IndexPageComponent implements OnInit {
+    public roleMenus!: RoleMenuAuth[]
+
+    constructor(private _rolService: RolService) {}
+
+    ngOnInit(): void {
+        this.getRoleMenuAuth()
+    }
+
+    getRoleMenuAuth() {
+        this._rolService.getRoleMenuAuth().subscribe({
+            next: ({ role_menu }) => {
+                this.roleMenus = role_menu
+            },
+            error: (err) => {},
+        })
+    }
 }
